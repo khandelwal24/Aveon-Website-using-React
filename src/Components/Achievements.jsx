c88b695a1ddb6844957fafd538c499fc624db017
Harshwardhan Khandelwal <khandelwalharsh0024@gmail.com>
2024-06-06 10:14:19 +0530

    changes

--- src/Components/Achievements.jsx
+++ src/Components/Achievements.jsx
@@ -17,7 +17,7 @@ function Achievements() {
                        return(
                         <div key={i} className="grid sm:grid-cols-2 grid-cols-1 gap-3 max-w-[850px] bg-[#080f29] text-white mx-auto px-7 py-5 rounded-2xl items-center justify-center text-center justify-items-center border-[1px] border-b-orange-200" style={{boxShadow:'5px 5px 10px rgb(65, 80, 216),-5px -5px 10px rgb(90,89,208)'}} data-aos="flip-up" data-aos-duration="5000">
                         
-                        <img src={v.img} width={250} loading='lazy' className='rounded-full border-[3px] border-[#4150d8]' style={{boxShadow:'5px 3px 5px rgb(65, 80, 216)'}}/>
+                        <img src={v.img} width={250} className='rounded-full border-[2px] border-[#4150d8]' style={{boxShadow:'5px 3px 5px rgb(65, 80, 216)'}}/>
                         
                         <p className='text-lg'>
                         <span className='text-3xl font-bold block text-[#4150d8]' style={{fontFamily:'Orbitron'}}>{v.year}</span>
