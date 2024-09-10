f6a48083300e8f5151d92b652eaaecce8057c7f3
Harshwardhan Khandelwal <khandelwalharsh0024@gmail.com>
2024-09-10 20:05:19 +0530

    newacjhjshvcajc

--- src/Components/Navbar2.jsx
+++ src/Components/Navbar2.jsx
@@ -32,10 +32,10 @@ function Navbar2() {
                 <img src={logo} alt='logo' className="h-12"/>
             </div>
             <div className="text-2xl space-x-5 lg:block hidden" style={{fontFamily:'Orbitron'}}>
-                <a href="/#Home" className="hover:text-gray-500 hover:border-b-[2px] border-white hover:transition-all ease-in-out duration-75 ">Home</a>
-                <a href="/#About" className="hover:text-gray-500 hover:border-b-[2px] border-white hover:transition-all ease-in-out duration-75 ">About</a>
+                <NavLink to ={"/#Home"} className="hover:text-gray-500 hover:border-b-[2px] border-white hover:transition-all ease-in-out duration-75 ">Home</NavLink>
+                <NavLink to ={"/#About"} className="hover:text-gray-500 hover:border-b-[2px] border-white hover:transition-all ease-in-out duration-75 ">About</NavLink>
                 <NavLink to={"/Team"} className="hover:text-gray-500 hover:border-b-[2px] border-white hover:transition-all ease-in-out duration-75">Team</NavLink>
-                <a href="/#Sponsor" className="hover:text-gray-500 hover:border-b-[2px] border-white hover:transition-all ease-in-out duration-75">Sponsors</a>
+                <NavLink to ={"/#Sponsor"} className="hover:text-gray-500 hover:border-b-[2px] border-white hover:transition-all ease-in-out duration-75">Sponsors</NavLink>
                 <NavLink to={"/Gallery"}  className="hover:text-gray-500 hover:border-b-[2px] border-white hover:transition-all ease-in-out duration-75">Gallery</NavLink>
             </div>
 
@@ -48,10 +48,10 @@ function Navbar2() {
 
         <div className= {Nav ? 'fixed left-0 top-0 w-[100%] h-full lol rounded-e-xl text-center ease-in-out duration-300 opacity-95': 'bg-[#080f29] ease-in-out duration-300 text-center fixed -left-40 '} style={{fontFamily:'Orbitron',padding:'5px'}} >
         <img src={logo} alt='logo' width={200} className= {`text-3xl font-bold text-white mb-6 px-4 py-[14px] ${ !Nav ? 'hidden' : 'block'} `}/>
-          <a href="/#Home" className=" border-b-[1px] rounded-xl border-white block py-3 px-2 mb-3 hover:bg-[#213068] hover:transition-all ease-in-out duration-75">Home</a>
-          <a href="/#About" className=" border-b-[1px] rounded-xl border-white block py-3 px-2 mb-3 hover:bg-[#213068] hover:transition-all ease-in-out duration-75">About</a>
+          <NavLink to={"/#Home"} className=" border-b-[1px] rounded-xl border-white block py-3 px-2 mb-3 hover:bg-[#213068] hover:transition-all ease-in-out duration-75">Home</NavLink>
+          <NavLink to={"/#About"} className=" border-b-[1px] rounded-xl border-white block py-3 px-2 mb-3 hover:bg-[#213068] hover:transition-all ease-in-out duration-75">About</NavLink>
           <NavLink to={'/Team'} className=" border-b-[1px] rounded-xl border-white block py-3 px-2 mb-3 hover:bg-[#213068] hover:transition-all ease-in-out duration-75">Team</NavLink>
-          <a href="/#Sponsor" className=" border-b-[1px] rounded-xl border-white block py-3 px-2 mb-3 hover:bg-[#213068] hover:transition-all ease-in-out duration-75">Sponsors</a>
+          <NavLink to={"/#Sponsor"} className=" border-b-[1px] rounded-xl border-white block py-3 px-2 mb-3 hover:bg-[#213068] hover:transition-all ease-in-out duration-75">Sponsors</NavLink>
           <NavLink to={'/Gallery'} className=" border-b-[1px] rounded-xl border-white block py-3 px-2 mb-3 hover:bg-[#213068] hover:transition-all ease-in-out duration-75">Gallery</NavLink>
           <a href={pdf} download="Team Aveon Broucher.pdf" className={`p-2.5 bg-gray-600 text-white rounded-full mt-7 border-2 border-white ${(Nav) ? 'block' : 'hidden'} hover:bg-gray-500`}>Download Broucher <i class="fa-solid fa-download"></i> </a>
       </div>
